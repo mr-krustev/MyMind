10b041c13d05a59f8b512f9f64d3eda00dd252e9
mr.krustev <mr.krustev1994@gmail.com>
2016-02-24 14:48:58 +0200

    Implemented ITestsServices and TestsServices.

---
+++ Source/Services/RightoGo.Services.Data/Contracts/ITestsServices.cs
@@ -0,0 +1,19 @@
+﻿namespace RightoGo.Services.Data.Contracts
+{
+    using System.Linq;
+
+    using RightoGo.Data.Models;
+
+    public interface ITestsServices
+    {
+        IQueryable<Test> GetAll();
+
+        IQueryable<Test> GetById(int id);
+
+        void Add(Test test);
+
+        void Update(Test test);
+
+        void Delete(Test test);
+    }
+}
---
+++ Source/Services/RightoGo.Services.Data/TestsServices.cs
@@ -0,0 +1,47 @@
+﻿namespace RightoGo.Services.Data
+{
+    using System;
+    using System.Linq;
+
+    using Contracts;
+    using RightoGo.Data.Common;
+    using RightoGo.Data.Models;
+
+    public class TestsServices : ITestsServices
+    {
+        private IDbRepository<Test> tests;
+
+        public TestsServices(IDbRepository<Test> tests)
+        {
+            this.tests = tests;
+        }
+
+        public void Add(Test test)
+        {
+            this.tests.Add(test);
+            this.tests.Save();
+        }
+
+        public void Delete(Test test)
+        {
+            this.tests.Delete(test);
+            this.tests.Save();
+        }
+
+        public IQueryable<Test> GetAll()
+        {
+            return this.tests.All();
+        }
+
+        public IQueryable<Test> GetById(int id)
+        {
+            return this.tests.All().Where(t => t.Id == id);
+        }
+
+        public void Update(Test test)
+        {
+            this.tests.Update(test);
+            this.tests.Save();
+        }
+    }
+}
